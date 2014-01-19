31c24e84e6851fd281d5c465c2d0a2af6d5c2a9c
David Goldschmidt <dmgoldschmidt@gmail.com>
2014-01-19 11:00:50 -0800

    Added transpose flag to SparseMatrix.  This allows pre and post multiplication by the transpose of a SparseMatrix, without actually having to create a separate object.

--- SparseMatrix.cc
+++ SparseMatrix.cc
@@ -1,25 +1,33 @@
 #include "Matrix.h"
 
-SparseMatrix::SparseMatrix(int m, int n, const matrix& A0) : _nrows(m), _ncols(n), _nentries(A0.nrows()), A(A0) {
+SparseMatrix::SparseMatrix(int m, int n, const matrix& A0) : _nrows(m), _ncols(n), _nentries(A0.nrows()), A(A0), transposed(0) {
     if(A.ncols() != 3) throw "SparseMatrix: input must have three columns\n";
   }
 
 matrix SparseMatrix::operator*(const matrix& M){ // premultiply M by sparse matrix
-  if(_ncols != M.nrows())throw "SparseMatrix premultiply: dimension error\n";
-  double zero = 0;
-  matrix M1(_nrows,M.ncols(),&zero);
+  int m = transposed? _ncols:_nrows;
+  int n = transposed? _nrows:_ncols;
+  if(n != M.nrows())throw "SparseMatrix premultiply: dimension error\n";
+  double zero = 0; // hokey! need another Matrix constructor
+  matrix M1(m,M.ncols(),&zero);
+  // cout << "transposed: "<<transposed<<" transposed^1: "<<(transposed^1)<<endl;
+  //cout <<"A:\n"<<A;
   
-  for(int m = 0;m < _nentries;m++){
-    int i = A(m,0);
-    int j = A(m,1);
-    for(int k = 0;k < M.ncols();k++) M1(i,k) += A(m,2)*M(j,k);
+  for(int k = 0;k < _nentries;k++){
+    //    cout << format("entry %d: i = %d, j = %d, %f\n",k,(int)A(k,transposed),(int)A(k,transposed^1),A(k,2));
+    int i = (int)A(k,transposed);
+    int j = (int)A(k,transposed^1);
+    for(int l = 0;l < M.ncols();l++){
+      M1(i,l) += A(k,2)*M(j,l);
+      //      cout << format("ans(%d,%d) += A(%d,%d)*M(%d,%d) = %f*%f\n",i,l,i,j,j,l,A(k,2),M(j,l));
+    }
   }
   return M1;
 }
- int SparseMatrix::row(int m) const {return A(m,0);}
- int SparseMatrix::col(int m) const {return A(m,1);}
- double SparseMatrix::entry(int m) const{return A(m,2);}
+int SparseMatrix::row(int m) const {return A(m,transposed);}
+int SparseMatrix::col(int m) const {return A(m,transposed^1);}
+double SparseMatrix::entry(int m) const{return A(m,2);}
    
- int SparseMatrix::nrows(void) const {return _nrows;}
- int SparseMatrix::ncols(void) const {return _ncols;}
- int SparseMatrix::nentries(void) const {return _nentries;}
+int SparseMatrix::nrows(void) const {return transposed? _ncols:_nrows;}
+int SparseMatrix::ncols(void) const {return transposed? _nrows:_ncols;}
+int SparseMatrix::nentries(void) const {return _nentries;}
